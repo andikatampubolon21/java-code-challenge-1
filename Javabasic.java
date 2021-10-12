bd7b313a194cdbe613de705a3dab652d615fa67d
andikatampubolon21 <91531340+andikatampubolon21@users.noreply.github.com>
2021-10-12 04:20:44 -0700

    Add files via upload

---
+++ Javabasic.java
@@ -0,0 +1,25 @@
+public class javaBasic {
+    // function is a method
+     public static void main(String[] args) {
+        // first Name ,lastnme,age,phone number,address,and other
+       
+        String firstName = "Andika";
+        String lastnme = "tampubolon";
+        int    age     = 21;
+        String phoneNumber = "082167111962";
+        String Address = "jln galang";
+
+        System.out.println("\n");
+        System.out.println("First name   = "+ firstName);
+        System.out.println("Last name    = "+ LastName );
+        System.out.println("Age          = "+ age);
+        System.out.println("Phone number = "+ phoneNumber);
+        System.out.println("Address      = "+Address);
+        System.out.println("Has GF ?     = "+hasGf);
+        System.out.println("\n");
+    }
+
+}        
+
+
+    
